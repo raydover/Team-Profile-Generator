a36ba21b555423714897dcb764cc0ab948beabf6
raydover <105178034+raydover@users.noreply.github.com>
2022-06-21 14:25:37 -0400

    commiting changes

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // Require modules from local directory
-const Employee = require('./lib/Employee.js');
-const Manager = require('./lib/Manager.js');
-const Engineer = require('./lib/Engineer.js');
-const Intern = require('./lib/Intern.js');
+// const Employee = require('./lib/Employee.js');
+// const Manager = require('./lib/Manager.js');
+// const Engineer = require('./lib/Engineer.js');
+// const Intern = require('./lib/Intern.js');
 
 
 
